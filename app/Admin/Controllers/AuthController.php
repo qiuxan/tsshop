805b988b907acbb353094c93721ff2a2989c6c84
qiuxan <qiuxan@gmail.com>
2019-09-09 12:21:28 +0000

    delete some useless bug fix

--- app/Admin/Controllers/AuthController.php
+++ app/Admin/Controllers/AuthController.php
@@ -1,82 +1,10 @@
 <?php
 
 namespace App\Admin\Controllers;
-use Illuminate\Http\Request;
-use Encore\Admin\Facades\Admin;
-use Encore\Admin\Form;
-use Encore\Admin\Layout\Content;
-use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Lang;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\Validator;
 
 use Encore\Admin\Controllers\AuthController as BaseAuthController;
 
 class AuthController extends BaseAuthController
 {
-    /**
-     * Handle a login request.
-     *
-     * @param Request $request
-     *
-     * @return mixed
-     */
-    public function postLogin(Request $request)
-    {
-        $credentials = $request->only([$this->username(), 'password']);
-        $remember = $request->get('remember', false);
 
-        /** @var \Illuminate\Validation\Validator $validator */
-        $validator = Validator::make($credentials, [
-            $this->username()   => 'required',
-            'password'          => 'required',
-        ]);
-
-        if ($validator->fails()) {
-            return back()->withInput()->withErrors($validator);
-        }
-
-        if ($this->guard()->attempt($credentials, $remember)) {
-            return $this->sendLoginResponse($request);
-        }
-
-
-        return back()->withInput()->withErrors([
-            $this->username() => $this->getFailedLoginMessage(),
-        ]);
-    }
-
-
-    /**
-     * Send the response after the user was authenticated.
-     *
-     * @param \Illuminate\Http\Request $request
-     *
-     * @return \Illuminate\Http\Response
-     */
-    protected function sendLoginResponse(Request $request)
-    {
-        admin_toastr(trans('admin.login_successful'));
-
-        $request->session()->regenerate();
-
-        return redirect('/admin');
-//        return redirect()->intended($this->redirectPath());
-    }
-
-
-    /**
-     * Get the post login redirect path.
-     *
-     * @return string
-     */
-    protected function redirectPath()
-    {
-        if (method_exists($this, 'redirectTo')) {
-            return $this->redirectTo();
-        }
-
-        return property_exists($this, 'redirectTo') ? $this->redirectTo : config('admin.route.prefix');
-    }
 }
