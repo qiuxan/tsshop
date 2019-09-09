7f86d412827a3e4d3f656c0209e77d4066c71a76
qiuxan <qiuxan@gmail.com>
2019-09-09 11:36:46 +0000

    fix the admin redirect bug

--- app/Admin/Controllers/AuthController.php
+++ app/Admin/Controllers/AuthController.php
@@ -1,10 +1,51 @@
 <?php
 
 namespace App\Admin\Controllers;
+use Illuminate\Http\Request;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Form;
+use Encore\Admin\Layout\Content;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
 
 use Encore\Admin\Controllers\AuthController as BaseAuthController;
 
 class AuthController extends BaseAuthController
 {
+    /**
+     * Handle a login request.
+     *
+     * @param Request $request
+     *
+     * @return mixed
+     */
+    public function postLogin(Request $request)
+    {
 
+//        dd(1);
+        $credentials = $request->only([$this->username(), 'password']);
+        $remember = $request->get('remember', false);
+
+        /** @var \Illuminate\Validation\Validator $validator */
+        $validator = Validator::make($credentials, [
+            $this->username()   => 'required',
+            'password'          => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return back()->withInput()->withErrors($validator);
+        }
+
+        if ($this->guard()->attempt($credentials, $remember)) {
+            return $this->sendLoginResponse($request);
+        }
+
+
+        return redirect('admin')->withInput()->withErrors([
+            $this->username() => $this->getFailedLoginMessage(),
+        ]);
+    }
 }
