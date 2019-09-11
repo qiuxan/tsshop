9c5c490b6db196e3f71fd1d4753420bf3b8cf863
qiuxan <qiuxan@gmail.com>
2019-09-11 06:06:37 +0000

    pack the controller function to class

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -3,85 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\OrderRequest;
-use App\Models\ProductSku;
 use App\Models\UserAddress;
 use App\Models\Order;
-use Carbon\Carbon;
-use App\Jobs\CloseOrder;
 use Illuminate\Http\Request;
-
-use App\Services\CartService;
+use App\Services\OrderService;
 
 
 
 
 class OrdersController extends Controller
 {
-    public function store(OrderRequest $request, CartService $cartService)
+
+    public function store(OrderRequest $request, OrderService $orderService)
     {
-        $user  = $request->user();
-        //start an db
-        $order = \DB::transaction(function () use ($user, $request,$cartService) {
-            $address = UserAddress::find($request->input('address_id'));
-            // update the last used date
-            $address->update(['last_used_at' => Carbon::now()]);
-            // create an order
-            $order   = new Order([
-                'address'      => [ // put info. into this order
-                    'address'       => $address->full_address,
-                    'zip'           => $address->zip,
-                    'contact_name'  => $address->contact_name,
-                    'contact_phone' => $address->contact_phone,
-                ],
-                'remark'       => $request->input('remark'),
-                'total_amount' => 0,
-            ]);
-            //link this order to the logged in user
-            $order->user()->associate($user);
-            //saved in db
-            $order->save();
+        $user    = $request->user();
+        $address = UserAddress::find($request->input('address_id'));
 
-            $totalAmount = 0;
-            $items       = $request->input('items');
-            // find the skus of this order
-            foreach ($items as $data) {
-                $sku  = ProductSku::find($data['sku_id']);
-                // create an order item to connect with this order
-                $item = $order->items()->make([
-                    'amount' => $data['amount'],
-                    'price'  => $sku->price,
-                ]);
-                $item->product()->associate($sku->product_id);
-                $item->productSku()->associate($sku);
-                $item->save();
-                $totalAmount += $sku->price * $data['amount'];
-                if ($sku->decreaseStock($data['amount']) <= 0) {
-                    throw new InvalidRequestException('Not enough stock');
-                }
-            }
-
-            //update the total amount of this order
-            $order->update(['total_amount' => $totalAmount]);
-
-            // remove the items from the chart
-
-            $skuIds = collect($request->input('items'))->pluck('sku_id')->all();
-            $cartService->remove($skuIds);
-
-//            $skuIds = collect($items)->pluck('sku_id');
-
-            $user->cartItems()->whereIn('product_sku_id', $skuIds)->delete();
-
-
-            return $order;
-        });
-
- //       $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
-
-
-        return $order;
+        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
     }
-
 
     public function index(Request $request)
     {
---
+++ app/Services/OrderService.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use App\Models\UserAddress;
+use App\Models\Order;
+use App\Models\ProductSku;
+use App\Exceptions\InvalidRequestException;
+use App\Jobs\CloseOrder;
+use Carbon\Carbon;
+
+class OrderService
+{
+    public function store(User $user, UserAddress $address, $remark, $items)
+    {
+        //
+        $order = \DB::transaction(function () use ($user, $address, $remark, $items) {
+            //
+            $address->update(['last_used_at' => Carbon::now()]);
+            //
+            $order   = new Order([
+                'address'      => [ //
+                    'address'       => $address->full_address,
+                    'zip'           => $address->zip,
+                    'contact_name'  => $address->contact_name,
+                    'contact_phone' => $address->contact_phone,
+                ],
+                'remark'       => $remark,
+                'total_amount' => 0,
+            ]);
+            //
+            $order->user()->associate($user);
+            //
+            $order->save();
+
+            $totalAmount = 0;
+            //
+            foreach ($items as $data) {
+                $sku  = ProductSku::find($data['sku_id']);
+                //
+                $item = $order->items()->make([
+                    'amount' => $data['amount'],
+                    'price'  => $sku->price,
+                ]);
+                $item->product()->associate($sku->product_id);
+                $item->productSku()->associate($sku);
+                $item->save();
+                $totalAmount += $sku->price * $data['amount'];
+                if ($sku->decreaseStock($data['amount']) <= 0) {
+                    throw new InvalidRequestException('该商品库存不足');
+                }
+            }
+            //
+            $order->update(['total_amount' => $totalAmount]);
+
+            //
+            $skuIds = collect($items)->pluck('sku_id')->all();
+            app(CartService::class)->remove($skuIds);
+
+            return $order;
+        });
+
+        //
+        dispatch(new CloseOrder($order, config('app.order_ttl')));
+
+        return $order;
+    }
+}
