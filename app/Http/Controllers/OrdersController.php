26b682179f13d4fa849ddd88172f98851e10fc57
qiuxan <qiuxan@gmail.com>
2019-09-14 07:09:13 +0000

    service in cloud did not work the same as local, go back to controller functions

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -7,6 +7,9 @@ use App\Models\UserAddress;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use App\Services\OrderService;
+use App\Models\ProductSku;
+use Carbon\Carbon;
+
 
 
 
@@ -14,13 +17,60 @@ use App\Services\OrderService;
 class OrdersController extends Controller
 {
 
-    public function store(OrderRequest $request, OrderService $orderService)
-    {
-        $user    = $request->user();
-        $address = UserAddress::find($request->input('address_id'));
+//    public function store(OrderRequest $request, OrderService $orderService)
+//    {
+//        $user    = $request->user();
+//        $address = UserAddress::find($request->input('address_id'));
+//
+//        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
+//    }
 
-        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
+    public function store(OrderRequest $request)
+    {
+        $user  = $request->user();
+        $order = \DB::transaction(function () use ($user, $request) {
+            $address = UserAddress::find($request->input('address_id'));
+            //
+            $address->update(['last_used_at' => Carbon::now()]);
+            $order   = new Order([
+                'address'      => [
+                    'address'       => $address->full_address,
+                    'zip'           => $address->zip,
+                    'contact_name'  => $address->contact_name,
+                    'contact_phone' => $address->contact_phone,
+                ],
+                'remark'       => $request->input('remark'),
+                'total_amount' => 0,
+            ]);
+            $order->user()->associate($user);
+            $order->save();
+
+            $totalAmount = 0;
+            $items       = $request->input('items');
+            foreach ($items as $data) {
+                $sku  = ProductSku::find($data['sku_id']);
+                $item = $order->items()->make([
+                    'amount' => $data['amount'],
+                    'price'  => $sku->price,
+                ]);
+                $item->product()->associate($sku->product_id);
+                $item->productSku()->associate($sku);
+                $item->save();
+                $totalAmount += $sku->price * $data['amount'];
+            }
+
+            $order->update(['total_amount' => $totalAmount]);
+
+            $skuIds = collect($items)->pluck('sku_id');
+            $user->cartItems()->whereIn('product_sku_id', $skuIds)->delete();
+
+            return $order;
+        });
+
+        return $order;
     }
+
+
 
     public function index(Request $request)
     {
