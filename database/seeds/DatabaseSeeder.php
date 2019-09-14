ab1e4651534714d9cce97884af5c98e5ae943a59
qiuxan <qiuxan@gmail.com>
2019-09-14 05:52:03 +0000

    seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
          $this->call(UsersTableSeeder::class);
          $this->call(UsersAddressTableSeeder::class);
+         $this->call(ProductsSeeder::class);
     }
 }
